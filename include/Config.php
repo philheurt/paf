869891590665c5251228610e2e04c421d3a808ef
dopzac <heurtauxphil@gmail.com>
2015-06-17 11:24:31 +0200

    First version, didn't modify everything

---
+++ include/Config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Database configuration
+ */
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'paf');
+
+define('DOCTOR_CREATED_SUCCESSFULLY', 0);
+define('DOCTOR_CREATE_FAILED', 1);
+define('DOCTOR_ALREADY_EXISTED', 2);
+?>
